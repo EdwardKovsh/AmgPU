3960a49a5b3b2349b5dcae889b458e94db7ab55c
Edward <kovshechnikov.eduard@mail.ru>
2022-10-03 15:35:50 +0300

    начало боковой панели

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
+// выдвижение и возврат боковой панели
 $(function(){
 	$('#pan-dri').click(function(){
-		$('.panel-drive').toggle('fast');
-		$('.panel-drive').toggleClass('active');
-		return 0;
+		$('.panel-drive').show(500);
 	});
 	$('#pan-not-dri').click(function(){
-		$('.panel-drive').toggle('fast');
-		$('.panel-drive').toggleClass('remote');
-		return 0;
+		$('.panel-drive').hide(500);
+	});
+});
+// наведение на кнопки внутри боковой панели
+$(function(){
+	$('p').mouseover(function(){
+		$('.panel-drive2').show(500);
+	});
+	$('p').mouseout(function(){
+		$('.panel-drive2').hide(500);
 	});
 });
