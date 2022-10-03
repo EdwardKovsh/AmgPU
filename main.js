ed5ee41b868b25472b91b576c13d3278d5a4aae5
Edward <kovshechnikov.eduard@mail.ru>
2022-10-03 22:12:00 +0300

    сделал функционал боковой панели, единственное надо редачить появление и уход панели

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
 // выдвижение и возврат боковой панели
 $(function(){
-	$('#pan-dri').click(function(){
+	$('#drive-panel').click(function(){
 		$('.panel-drive').show(500);
 	});
-	$('#pan-not-dri').click(function(){
+	$('#panel-back-drive').click(function(){
 		$('.panel-drive').hide(500);
 	});
 });
 // наведение на кнопки внутри боковой панели
-$(function(){
-	$('p').mouseover(function(){
-		$('.panel-drive2').show(500);
-	});
-	$('p').mouseout(function(){
-		$('.panel-drive2').hide(500);
-	});
-});
+let ada = document.getElementsByClassName("additionally")
+// let determinant = false;
+// только попробуйте это кто-то удалить
+// $(function(){
+// 	$('p').click(function(){
+// 		if (determinant == false){
+// 			$('.panel-inside').hide(500);
+
+// 			determinant = true;
+// 		};
+// 		$('.panel-inside').show(500);
+// 		determinant = false;
+// 	});
+// });
+function develop(num){
+	ada[0].style.display = "none";
+	ada[1].style.display = "none";
+	ada[2].style.display = "none";
+	ada[3].style.display = "none";
+	ada[4].style.display = "none";
+	ada[5].style.display = "none";
+	ada[6].style.display = "none";
+	ada[7].style.display = "none";
+	ada[8].style.display = "none";
+	ada[9].style.display = "none";
+	ada[num].style.display = "block";
+}
